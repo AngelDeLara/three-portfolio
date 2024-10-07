2bbe0802e8dc99d86ad57fa77c3868c56f7425f1
AngelDeLara <robertoangel19@gmail.com>
2024-10-06 22:42:41 -0600

    added HackerRoom model

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,14 +1,91 @@
+import { Canvas } from '@react-three/fiber'
+import { PerspectiveCamera } from '@react-three/drei';
+import HackerRoom from '../components/HackerRoom';
+import { Suspense } from 'react';
+import CanvasLoader from '../components/CanvasLoader';
+import { Leva, useControls } from 'leva';
+import { useMediaQuery } from 'react-responsive';
+import { calculateSizes } from '../constants';
+
 const Hero = () => {
+  const x = useControls('HackerRoom', {
+    positionX: {
+      value: 2.5,
+      min: -10,
+      max: 10,
+    },
+    positionY: {
+      value: 2.5,
+      min: -10,
+      max: 10,
+    },
+    positionZ: {
+      value: 2.5,
+      min: -10,
+      max: 10,
+    },
+    rotationX: {
+      value: 0,
+      min: -10,
+      max: 10,
+    },
+    rotationY: {
+      value: 0,
+      min: -10,
+      max: 10,
+    },
+    rotationZ: {
+      value: 0,
+      min: -10,
+      max: 10,
+    },
+    scale: {
+      value: 1,
+      min: 0.1,
+      max: 10,
+    },
+
+  })
+
+  const isSmall = useMediaQuery({ maxWidth: 440 })
+  const isMobile = useMediaQuery({maxWidth: 768})
+  const isTablet = useMediaQuery({ minWidth: 769, maxWidth: 1024})
+
+  const sizes = calculateSizes(isSmall, isMobile, isTablet)
+
   return (
     <section className="min-h-screen w-full flex flex-col relative">
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space 
         gap-3"
       >
-        <p className="sm:text-3xl txt-xl font-medium text-white text-center 
+        <p className="sm:text-3xl txt-2xl font-medium text-white text-center 
           font-generalsans"
         >
           Hi, I am Angel <span className="waving-hand">👋</span>
         </p>
+        <p className="hero_tag text-gray_gradient">Building Products & Brands</p>
+      </div>
+      <div className="w-full h-full absolute inset-0">
+        <Leva />
+        <Canvas className='w-full h-full'>
+          <Suspense fallback={<CanvasLoader />}>
+
+          <PerspectiveCamera 
+            makeDefault 
+            position={[0, 0, 30]} 
+          />
+
+          <HackerRoom 
+            position={[2, -8, 2]}  
+            rotation={[0, -Math.PI, 0]}
+            scale={isMobile ? 0.07 : 0.1}
+          />
+
+
+          <ambientLight intensity={1} />
+          <directionalLight position={[10, 10, 10]} intensity={0.5} />
+          </Suspense>
+        </Canvas>
       </div>
     </section>
   )
