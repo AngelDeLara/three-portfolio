55d352a0c150aad3de81d7ec1d7b5e7c0ae49915
AngelDeLara <robertoangel19@gmail.com>
2024-10-07 20:00:40 -0600

    add more models to Hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -6,6 +6,9 @@ import CanvasLoader from '../components/CanvasLoader';
 import { useMediaQuery } from 'react-responsive';
 import { calculateSizes } from '../constants';
 import Target from '../components/Target';
+import ReactLogo from '../components/ReactLogo';
+import Cube from '../components/Cube';
+import Rings from './Rings';
 
 const Hero = () => {
   const isSmall = useMediaQuery({ maxWidth: 440 })
@@ -44,6 +47,9 @@ const Hero = () => {
 
           <group>
             <Target position={sizes.targetPosition} />
+            <ReactLogo position={sizes.reactLogoPosition} />
+            <Cube position={sizes.cubePosition} />
+            <Rings position={sizes.ringPosition} />
           </group>
 
           <ambientLight intensity={1} />
